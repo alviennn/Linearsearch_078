69727a49d4e2d22a291a66bcd9bb8b2a44ef7754
alviennn <alvien.rnp@gmail.com>
2023-04-14 15:32:27 +0700

    menambahkan int main

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //Array to be search
-int n; // Number of elements in the array
-int i; // Index of array element
+int arr[20];
+int n;
+int i;
 
 void input()
 {
 	while (true)
 	{
-		cout << "Enter the number of element in the array: ";
+		cout << "enter the number of element in the array : ";
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
 		else
-			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+			cout << "\narray should have minimum 1 and maximum 20 element.\n\n";
 	}
 
-	// Accept array elements
-	cout << "Enter array elements\n";
-	cout << "--------------------\n";
+
+	cout << "\n---------------------\n";
+	cout << " enter array element \n";
+	cout << "---------------------\n";
 	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << "> ";
@@ -27,40 +28,42 @@ void input()
 	}
 }
 
-void LinearSearch()
+
+
+void linesearch()
 {
 	char ch;
-	int comparisons; // Number of comparisons
+	int comparisons;
 
 	do
 	{
-		// Accept the number to be searched
-		cout << "\nEnter the element you want to search: ";
+		cout << "\nenter the element you want to search: ";
 		int item;
 		cin >> item;
 
 		comparisons = 0;
-		for (i = 0; i < n; i++) // Langkah 2, 3, dan 4
+		for (i = 0; i < n; i++)
 		{
 			comparisons++;
-			if (arr[i] == item); //  Langkah 5A found
+			if (arr[i] == item)
 			{
-				cout << "\n" << item << "found at position " << (i + 1) << endl;
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
 				break;
 			}
+
 		}
 
-		if (i == n) // Langkah 5B not found
+		if (i == n)
 			cout << "\n" << item << "not found in the array\n";
-		cout << "\nNumber of comparisons: " << comparisons << endl;
-		
-		cout << "\nContinue search (y/n): ";
+		cout << "\nnumber of comparisons: " << comparisons << endl;
+
+		cout << "\ncontinue search (y/n): ";
 		cin >> ch;
-	} while ((ch == 'y') || (ch == 'y'));
+	} while ((ch == 'y') || (ch == 'Y'));
 }
 
 int main()
 {
 	input();
-	LinearSearch();
+	linesearch();
 }
